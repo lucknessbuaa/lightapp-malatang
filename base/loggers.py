62f53f303b4f7dabaddfd71bfd17199c8adec11d
jarvys <yuhan534@126.com>
2014-10-01 11:18:05 +0800

    add logger config

---
+++ base/loggers.py
@@ -0,0 +1,56 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+LOGGING = {
+    'version': 1,
+    'dusable_existing_loggers': True,
+    'formatters': {
+        'normal': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(module)s %(message)s'
+            }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'django': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'when': 'D',
+            'interval': 1,
+            'backupCount': 5,
+            'filename': os.path.join(BASE_DIR, 'logs/django.log'),
+            'formatter': 'normal'
+        },
+        'app': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'when': 'D',
+            'interval': 1,
+            'backupCount': 5,
+            'filename': os.path.join(BASE_DIR, 'logs/app.log'),
+            'formatter': 'normal'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['django'],
+            'propagate': False,
+            'level': 'DEBUG'
+        },
+        '': {
+            'handlers': ['app'],
+            'level': 'DEBUG'
+        }
+    }
+}
