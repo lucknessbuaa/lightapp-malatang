06c6f530fcb3700dab1b9f2dc80ae1e0d860a7c6
jarvys <yuhan534@126.com>
2014-10-01 16:13:53 +0800

    save

--- base/common.py
+++ base/common.py
@@ -78,7 +78,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "assets")
+    os.path.join(BASE_DIR, "assets"),
 )
 
 TEMPLATE_DIRS = (
