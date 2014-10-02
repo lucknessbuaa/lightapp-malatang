f0f6247fccf36900baeec76b2c85d4a17c9fd506
PopPopRet <lance.through@the.heart>
2014-10-02 21:48:11 +0800

    save

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 
 urlpatterns = patterns('app.views',
     url(r'^$', 'index'),
+    url(r'^seatOrder$', 'seatOrder'),
+    url(r'^dishes$', 'dishes'),
+    url(r'^order$', 'order'),
+    url(r'^myOrder$', 'myOrder'),
 )
--- app/views.py
+++ app/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'app/index.html')
+
+def seatOrder(request):
+	return render(request, 'app/seatOrder.html')
+
+def dishes(request):
+	return render(request, 'app/dishes.html')
+
+def order(request):
+	return render(request, 'app/order.html')
+
+def myOrder(request):
+	return render(request, 'app/myOrder.html')
