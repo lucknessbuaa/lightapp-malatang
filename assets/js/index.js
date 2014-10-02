2c2ebbb1bf6a3e3233cc14c138ed3c7bdc13c0e3
PopPopRet <lance.through@the.heart>
2014-10-02 17:51:50 +0800

    save

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+$(function(){
+	$("a.seat_order").animate({left: 0},400);
+	$("a.dishes").animate({left: 0},600);
+	$("a.order").animate({left: 0},800);
+	$("a.my_order").animate({left: 0},900);
+
+	// var width = $("body").width();
+	// var height = $("body").height();
+
+	// $('a.card').click(function(){
+	// 	$(this).find('img').css({
+	// 		"position": "fixed",
+	// 	}).animate({
+	// 		"top": 0,
+	// 		"left": 0,
+	// 		"width": width,
+	// 		"height": height,
+	// 		"z-index": "9999",
+	// 		"margin": "0",
+	// 		"padding": "0",
+	// 		"background": "inherit",
+	// 		"background-color":"white"
+	// 	},600);
+	// });
+});
